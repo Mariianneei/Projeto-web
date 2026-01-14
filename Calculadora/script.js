4c04c822b037fd03b12e475bf9800a001aea3afe
Marianne Barboza <142535417+Mariianneei@users.noreply.github.com>
2026-01-13 22:02:25 -0300

    Add files via upload

---
+++ Calculadora/script.js
@@ -0,0 +1,17 @@
+function soma(){
+    var numero1 = parseFloat (document.getElementById("n1").value)
+    var numero2 = parseFloat (document.getElementById("n2").value)
+    var resp = document.getElementById("resposta")
+    //nao pode deixar console log nos codigos
+    //console.log(typeof numero1, typeof numero2)
+
+    resp.textContent = numero1 + numero2
+}
+function sub(){
+var numero3 = parseFloat(document.getElementById("n3").value)
+var numero4 = parseFloat(Document.getElementById("n4").value)
+
+
+
+
+}
